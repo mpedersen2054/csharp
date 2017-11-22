79dd78a131dba6e243234977c594d5350ddd5ef7
Matt Pedersen <mpedersen2054@gmail.com>
2017-11-22 16:49:35 -0500

    add TypeConversion

---
+++ dotnet_basics_udemy/TypeConversion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // implicit
+            byte b1 = 1;
+            int i1 = b1;
+            System.Console.WriteLine(i1);
+
+            // explicit
+            int i2 = 1;
+            byte b2 = (byte)i2;
+            System.Console.WriteLine(b2);
+
+            // non-compatible types
+            string s1 = "25";
+            int num = Convert.ToInt32(s1);
+            System.Console.WriteLine(num);
+
+            // use try block to handle exception
+            // prevents the application for crashing
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                System.Console.WriteLine("The Number is: {0}", b);
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("The Number could not be converted to a byte...");
+                throw;
+            }
+        }
+    }
+}
